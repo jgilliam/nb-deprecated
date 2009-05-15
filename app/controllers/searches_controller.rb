751be08bda24d04a69922311617a1f7fda0f68c0
Jim Gilliam <jim@gilliam.com>
2009-05-15 12:56:13 -0700

    prompts people to enter a search term if they do a blank search

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,13 +2,18 @@ class SearchesController < ApplicationController
   
   def index
     @page_title = t('searches.index.title', :government_name => current_government.name)
+    @priorities = nil
     if params[:q]
       query = params[:q]
-      @page_title = t('searches.results', :government_name => current_government.name, :query => params[:q])
-      @priorities = Priority.search(query, :match_mode => :extended, :page => params[:page], :per_page => 25)
-      @documents = Document.search(query, :match_mode => :extended, :page => params[:page], :per_page => 1)
-      @points = Point.search(query, :match_mode => :extended, :page => params[:page], :per_page => 1)
-      get_endorsements
+      @page_title = t('searches.results', :government_name => current_government.name, :query => query)
+      if query.blank?
+        flash.now[:error] = t('briefing.search.blank')
+      else
+        @priorities = Priority.search(query, :match_mode => :extended, :page => params[:page], :per_page => 25)
+        @documents = Document.search(query, :match_mode => :extended, :page => params[:page], :per_page => 1)
+        @points = Point.search(query, :match_mode => :extended, :page => params[:page], :per_page => 1)
+        get_endorsements        
+      end
     end
     respond_to do |format|
       format.html
@@ -21,15 +26,19 @@ class SearchesController < ApplicationController
     @page_title = t('briefing.search.points.title', :government_name => current_government.name, :briefing_name => current_government.briefing_name)
     @points = nil
     if params[:q]
-      @page_title = t('briefing.search.points.results', :briefing_name => current_government.briefing_name, :query => params[:q])
       query = params[:q]
-      @points = Point.search(query, :match_mode => :extended, :page => params[:page], :per_page => 15)
-      @priorities = Priority.search(query, :match_mode => :extended, :page => params[:page], :per_page => 1)
-      @documents = Document.search(query, :match_mode => :extended, :page => params[:page], :per_page => 1)
-      @qualities = nil
-      if logged_in? and @points.any? # pull all their qualities on the points shown
-        @qualities = PointQuality.find(:all, :conditions => ["point_id in (?) and user_id = ? ", @points.collect {|c| c.id if c.class == Point},current_user.id])
-      end      
+      @page_title = t('briefing.search.points.results', :briefing_name => current_government.briefing_name, :query => query)
+      if query.blank?
+        flash.now[:error] = t('briefing.search.blank')
+      else
+        @points = Point.search(query, :match_mode => :extended, :page => params[:page], :per_page => 15)
+        @priorities = Priority.search(query, :match_mode => :extended, :page => params[:page], :per_page => 1)
+        @documents = Document.search(query, :match_mode => :extended, :page => params[:page], :per_page => 1)
+        @qualities = nil
+        if logged_in? and @points.any? # pull all their qualities on the points shown
+          @qualities = PointQuality.find(:all, :conditions => ["point_id in (?) and user_id = ? ", @points.collect {|c| c.id if c.class == Point},current_user.id])
+        end    
+      end  
     end
     respond_to do |format|
       format.html
@@ -42,12 +51,15 @@ class SearchesController < ApplicationController
     @page_title = t('briefing.search.documents.title', :government_name => current_government.name, :briefing_name => current_government.briefing_name)
     @documents = nil
     if params[:q]
-      @page_title = t('briefing.search.documents.results', :briefing_name => current_government.briefing_name, :query => params[:q])
       query = params[:q]
-      @documents = Document.search(query, :match_mode => :extended, :page => params[:page], :per_page => 15)
-      @priorities = Priority.search(query, :match_mode => :extended, :page => params[:page], :per_page => 1)
-      @points = Point.search(query, :match_mode => :extended, :page => params[:page], :per_page => 1)
-      @qualities = nil
+      if query.blank?
+        flash.now[:error] = t('briefing.search.blank')
+      else      
+        @page_title = t('briefing.search.documents.results', :briefing_name => current_government.briefing_name, :query => query)
+        @documents = Document.search(query, :match_mode => :extended, :page => params[:page], :per_page => 15)
+        @priorities = Priority.search(query, :match_mode => :extended, :page => params[:page], :per_page => 1)
+        @points = Point.search(query, :match_mode => :extended, :page => params[:page], :per_page => 1)
+      end
     end
     respond_to do |format|
       format.html
