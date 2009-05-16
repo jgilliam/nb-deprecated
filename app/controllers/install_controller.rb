82496fd0fbef47af2fb6b4703e1442881b9abedd
Jim Gilliam <jim@gilliam.com>
2009-05-16 16:19:37 -0700

    changed up the way the database is loaded in multi government mode

--- app/controllers/install_controller.rb
+++ app/controllers/install_controller.rb
@@ -2,7 +2,7 @@ class InstallController < ApplicationController
 
   layout false
 
-  skip_before_filter :hijack_db
+  skip_before_filter :set_facebook_session
   skip_before_filter :check_subdomain
   skip_before_filter :check_blast_click
   skip_before_filter :check_priority
@@ -11,16 +11,9 @@ class InstallController < ApplicationController
   skip_before_filter :update_loggedin_at
   skip_before_filter :check_facebook
   
-  def load_db
-    current_government.switch_db_back
-    # this will generate an error if the database already exists
-    Government.connection.execute("CREATE DATABASE #{current_government.db_name} character SET utf8 COLLATE utf8_general_ci")
-    current_government.switch_db
-    file = "#{RAILS_ROOT}/db/schema.rb"
-    load(file)
-    User.connection.execute("ALTER TABLE rankings ENGINE=MYISAM")
-    User.connection.execute("ALTER TABLE user_rankings ENGINE=MYISAM")    
-    User.connection.execute("ALTER TABLE pictures ENGINE=MYISAM")
+  def load_first_user
+    redirect_to "/" and return if User.first # if there's already a user account, don't do anything
+    
     @user = User.create(:login => current_government.admin_name, :first_name => "Administrator", :last_name => "Account", :email => current_government.admin_email, :password => "blahblah", :password_confirmation => "blahblah", :is_admin => true)
     @user.reset_password
     CapitalGovernmentNew.create(:recipient => @user, :amount => 5)
