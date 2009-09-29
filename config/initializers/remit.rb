7788c24618eafe446fc6e09d3e3187496fbf5ff9
Jim Gilliam <jim@gilliam.com>
2009-09-29 07:00:15 -0700

    ditched remit

--- config/initializers/remit.rb
+++
@@ -1,5 +0,0 @@
-config_file = File.join(Rails.root, 'config', 'amazon_fps.yml')
-config = YAML.load_file(config_file)[RAILS_ENV].symbolize_keys
-
-FPS_ACCESS_KEY = config[:access_key]
-FPS_SECRET_KEY = config[:secret_key]
