484144aa489f31220c9d9aec445c3cebb8013db3
Jim Gilliam <jim@gilliam.com>
2009-10-05 10:58:33 -0700

    replaced will_paginate plugin with gem version, the fork from dweinand that supports active resource

--- config/environment.rb
+++ config/environment.rb
@@ -19,6 +19,7 @@ Rails::Initializer.run do |config|
   config.gem 'oauth', :version => '>= 0.3.1'
   config.gem 'hpricot', :version => '>= 0.6'
   config.gem 'liquid'
+  config.gem 'dweinand-will_paginate', :lib => 'will_paginate', :source => 'http://gems.github.com/'
   #config.gem 'curb', :version => '0.1.4'
   
   # Settings in config/environments/* take precedence over those specified here.
