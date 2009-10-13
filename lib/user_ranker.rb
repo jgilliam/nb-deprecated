f1592b582980685c2679553ade1f9000c06292eb
Jim Gilliam <jim@gilliam.com>
2009-10-13 08:01:51 -0700

    moved charting stuff out of rake task and into delayed job rankers

--- lib/user_ranker.rb
+++ lib/user_ranker.rb
@@ -4,6 +4,7 @@ class UserRanker
     Government.current = Government.all.last
     
     if Government.current.is_branches?
+
       for branch in Branch.all
         # get the last version # for the different time lengths
         v = branch.user_rankings.find(:all, :select => "max(version) as version")[0]
@@ -112,6 +113,62 @@ class UserRanker
     end
     
     User.connection.execute("update users set position = 0 where capitals_count = 0;")
+
+    # now check to see if the charts have been updated for the day
+    
+    date = Time.now-4.hours-1.day
+    start_date = date.year.to_s + "-" + date.month.to_s + "-" + date.day.to_s
+    end_date = (date+1.day).year.to_s + "-" + (date+1.day).month.to_s + "-" + (date+1.day).day.to_s
+    
+    if Government.current.is_branches?
+      if BranchUserChart.count(:conditions => ["date_year = ? and date_month = ? and date_day = ?", date.year, date.month, date.day]) == 0  # check to see if it's already been done for yesterday
+        for branch in Branch.all
+          users = branch.users.active.at_least_one_endorsement.by_ranking.all
+          for p in users
+            # find the ranking
+            r = branch.user_rankings.find(:all, :conditions => ["user_id = ? and branch_user_rankings.created_at between ? and ?",p.id, start_date,end_date], :order => "created_at desc",:limit => 1)
+            if r.any?
+              c = branch.user_charts.find_by_date_year_and_date_month_and_date_day_and_user_id(date.year,date.month,date.day,p.id)
+              if not c
+                c = branch.user_charts.new(:user => p, :date_year => date.year, :date_month => date.month, :date_day => date.day)
+              end
+              c.position = r[0].position
+              c.save
+            end
+          end    
+        end
+      end
+    end
+
+    if UserChart.count(:conditions => ["date_year = ? and date_month = ? and date_day = ?", date.year, date.month, date.day]) == 0  # check to see if it's already been done for yesterday
+      users = User.active.at_least_one_endorsement.by_ranking.all
+      for p in users
+        # find the ranking
+        r = p.rankings.find(:all, :conditions => ["user_rankings.created_at between ? and ?",start_date,end_date], :order => "created_at desc",:limit => 1)
+        if r.any?
+          c = p.charts.find_by_date_year_and_date_month_and_date_day(date.year,date.month,date.day)
+          if not c
+            c = UserChart.new(:user => p, :date_year => date.year, :date_month => date.month, :date_day => date.day)
+          end
+          c.position = r[0].position
+          up_capitals = Capital.find(:all, :conditions => ["((recipient_id = ? and amount > 0) or (sender_id = ? and amount < 0)) and created_at between ? and ?", p.id, p.id, start_date, end_date])
+          c.up_count = 0
+          for cap in up_capitals
+            c.up_count += cap.amount.abs
+          end
+          down_capitals = Capital.find(:all, :conditions => ["((recipient_id = ? and amount < 0) or (sender_id = ? and amount > 0)) and created_at between ? and ?", p.id, p.id, start_date, end_date])
+          c.down_count = 0
+          for cap in down_capitals
+            c.down_count += cap.amount.abs
+          end          
+          c.volume_count = c.up_count + c.down_count
+          c.save
+          if p.created_at+2.days > Time.now # within last two days, check to see if we've given them their priroity debut activity
+            ActivityUserRankingDebut.create(:user => p, :user_chart => c) unless ActivityUserRankingDebut.find_by_user_id(p.id)
+          end          
+        end
+      end
+    end
 
     Delayed::Job.enqueue UserRanker.new, -1, 51.minutes.from_now
     
