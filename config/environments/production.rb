a85837eb0c9ffb7235b1bc7fd4c9a70b7a74c582
Jim Gilliam <jim@gilliam.com>
2009-10-21 16:37:50 -0700

    configuration for heroku memcached

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,12 @@
 # The production environment is meant for finished, "live" apps.
 # Code is not reloaded between requests
 config.cache_classes = true
-config.cache_store = :mem_cache_store, 'localhost:11211'
+
+if ENV['MEMCACHE_SERVERS']
+  config.cache_store = :mem_cache_store, ENV['MEMCACHE_SERVERS'], { :namespace => ENV['MEMCACHE_NAMESPACE']}
+else
+  config.cache_store = :mem_cache_store, 'localhost:11211'
+end
 
 # Use a different logger for distributed setups
 # config.logger = SyslogLogger.new
